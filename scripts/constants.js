1f704855f05128ff1c79c966a8fce0ddc2849919
Russell Larkin <russell.larkin@gmail.com>
2023-04-06 01:51:51 +0100

    Update required core version

--- scripts/constants.js
+++ scripts/constants.js
@@ -15,7 +15,7 @@ export const CORE_MODULE = {
 /**
  * Core module version required by the system module
  */
-export const REQUIRED_CORE_MODULE_VERSION = '1.2'
+export const REQUIRED_CORE_MODULE_VERSION = '1.3'
 
 /**
  * Action icons
