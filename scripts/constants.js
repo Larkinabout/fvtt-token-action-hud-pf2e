afa0d2578bf116008af30ddf7f852aca37c513b7
Silvertower <alankmiles@gmail.com>
2026-04-22 03:51:46 +0000

    Add support for FVTT v14.0 and PF2E System v8.0.0

--- scripts/constants.js
+++ scripts/constants.js
@@ -15,7 +15,7 @@ export const CORE_MODULE = {
 /**
  * Core module version required by the system module
  */
-export const REQUIRED_CORE_MODULE_VERSION = '2.0'
+export const REQUIRED_CORE_MODULE_VERSION = '2.1'
 
 /**
  * Damage type icons
